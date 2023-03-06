987d9094f059d6fe2db3bf38992588613d71ffa9
PhungCongTuyen <92852184+PhungCongTuyen@users.noreply.github.com>
2023-03-06 17:41:00 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
-(function getMockData() {
+window.onload = function () {
+  const getMockData = () => {
     fetch("/data.json")
       .then((r) => r.json())
       .then(async (data) => {
         mapData(data);
       });
-  })();
-  
-  function handleClick(id) {
+  };
+
+  getMockData();
+
+  const handleClick = (event) => {
+    console.log(event.target);
+    const index = event.target.id;
     const a = document.getElementsByClassName("accordion__wrapper");
-    for (let i = 0; i < a.length; i++) {
-      if (i === id) {
-        a[i].classList.toggle("accordion__active");
-      } else a[i].classList.remove("accordion__active");
+    for (const item of a) {
+      item.classList.remove("accordion__active");
     }
-  }
-  
-  function mapData(data) {
+    a[index].classList.toggle("accordion__active");
+  };
+
+  const mapData = (data) => {
     document.getElementById("accordion").innerHTML = data
       .map(
         (item, index) =>
           `
-            <div class="accordion__wrapper">
+            <div class="accordion__wrapper" id="${index}">
               <div 
                 class="accordion__label" 
-                onclick="handleClick(${index})"
               >
                 <div class="accordion__title">${item.title}</div>
               </div>
@@ -32,5 +35,7 @@
           `
       )
       .join("");
-  }
-  
+  };
+
+  document.getElementById("accordion").addEventListener("click", handleClick);
+};
